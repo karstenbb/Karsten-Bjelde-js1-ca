7af8c5197e8b238c8cbb8bfafd6fc58ba09d14d1
karstenbb <57747742+karstenbb@users.noreply.github.com>
2020-02-25 10:03:44 +0100

    Add files via upload

--- javascript-1-ca-master/js/about.js
+++ javascript-1-ca-master/js/about.js
@@ -5,8 +5,8 @@ function replaceTheReplaced() {
   let h1text = h1Tag.innerText;
   let replaceh1 = h1text.replace("The", "Replaced");
   h1Tag.innerText = replaceh1;
-  theDiv.innerText = theDiv.innerText.replace(/the/g, "replace");
-  theDiv.innerText = theDiv.innerText.replace(/The/g, "Replace");
+  theDiv.innerText = theDiv.innerText.replace(/ the /g, " replace ");
+  theDiv.innerText = theDiv.innerText.replace(/ The /g, " Replace ");
 }
 
 setTimeout(replaceTheReplaced, 4000);
